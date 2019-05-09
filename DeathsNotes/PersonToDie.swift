d0e5e96f69a05e1318e47e6e8bfa393053ff5ea3
aalokhin <n.alokhina@gmail.com>
2019-05-09 18:39:53 +0300

    nothing works

---
+++ DeathsNotes/PersonToDie.swift
@@ -0,0 +1,15 @@
+//
+//  PersonToDie.swift
+//  DeathsNotes
+//
+//  Created by Anastasiia ALOKHINA on 5/9/19.
+//  Copyright © 2019 Anastasiia ALOKHINA. All rights reserved.
+//
+
+import Foundation
+
+struct PersonToDie{
+    var Name : String
+    var date : Date
+    var deathScenario : String
+}
